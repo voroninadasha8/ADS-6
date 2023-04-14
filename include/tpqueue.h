ce2dc26b18fe874c9ec8ea5f498114855cf4eb55
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-04-14 22:11:35 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,46 +4,47 @@
 #include <string>
 template<typename T, int size>
 class TPQueue {
+
   private:
-    T* arr;
-    int first, last, count;
+      T* arr;
+      int first, last, count;
     
   public:
-    TPQueue() : first(0), last(0), count(0) {
-      arr = new T[size];
-    }
-    ~TPQueue() {
-      delete[] arr;
-    }
-    bool isEmpty() const {
-      return count == 0;
-    }
-    bool isFull() const {
-      return size == count;
-    }
-    void push(const T& value) {
-      if (isFull()) {
-        throw std::string("Full!");
-      } else {
-        int end = last;
-        for (int i = last; i > first; i--) {
-          if (arr[i - 1].prior < value.prior) {
-            end = i - 1;
-            arr[i % size] = arr[i - 1];
+      TPQueue() : first(0), last(0), count(0) {
+        arr = new T[size];
+      }
+      ~TPQueue() {
+        delete[] arr;
+      }
+      bool isEmpty() const {
+        return count == 0;
+      }
+      bool isFull() const {
+        return size == count;
+      }
+      void push(const T& value) {
+        if (isFull()) {
+          throw std::string("Full!");
+        } else {
+          int end = last;
+          for (int i = last; i > first; i--) {
+            if (arr[i - 1].prior < value.prior) {
+              end = i - 1;
+              arr[i % size] = arr[i - 1];
+            }
           }
+          arr[end % size] = value;
+          count++;
+          last++;
         }
-        arr[end % size] = value;
-        count++;
-        last++;
       }
-    }
-    const T& pop() {
-      if (isEmpty()) {
-        throw std::string("Empty!");
+      const T& pop() {
+        if (isEmpty()) {
+          throw std::string("Empty!");
+        }
+        count--;
+        return arr[first++ % size];
       }
-      count--;
-      return arr[first++ % size];
-    }
 };
 
 struct SYM {
